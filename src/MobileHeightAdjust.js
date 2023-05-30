1a1739a94b10aee94eb58c2d8fd97eafbce8438e
Julieta Fredes <julietal.fredes@gmail.com>
2023-05-30 16:17:45 +0200

    add comments

--- src/MobileHeightAdjust.js
+++ src/MobileHeightAdjust.js
@@ -3,33 +3,34 @@ import { tf_debounce } from "@andresclua/debounce-throttle";
 
 class MobileHeightAdjust {
   constructor() {
-    this.vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty("--vh", `${this.vh}px`);
+    this.vh = window.innerHeight * 0.01; // Calculate 1% of the window height
+    document.documentElement.style.setProperty("--vh", `${this.vh}px`); // Set the custom CSS variable "--vh" to the calculated value
 
     this.jsUtil = new JSUTIL();
     this.events();
   }
 
   events() {
-    this.clickEvent = this.setHeight();
+    this.setHeight(); // Call the method directly instead of assigning it as an event listener
   }
 
-  setHeight = () => {
+  setHeight() {
     if (this.jsUtil.getTypeDevice("touch")) {
-      document.documentElement.style.setProperty("--vh", `${this.vh}px`);
+      // Check if the device is touch-enabled
+      document.documentElement.style.setProperty("--vh", `${this.vh}px`); // Set the custom CSS variable "--vh" to the calculated value
     }
 
     window.addEventListener(
       "resize",
       tf_debounce((e) => {
-        this.vh = window.innerHeight * 0.01;
-        document.documentElement.style.setProperty("--vh", `${this.vh}px`);
+        this.vh = window.innerHeight * 0.01; // Recalculate 1% of the window height
+        document.documentElement.style.setProperty("--vh", `${this.vh}px`); // Set the custom CSS variable "--vh" to the recalculated value
       }, 100)
     );
-  };
+  }
 
   destroy() {
-    this.clickEvent = {};
+    window.removeEventListener("resize", this.setHeight); // Remove the event listener correctly
   }
 }
 export default MobileHeightAdjust;
